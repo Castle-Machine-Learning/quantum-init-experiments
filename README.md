b38a404f0f9b2c8179f2926b02e2a73cba01495d
moritz@wolter.tech <moritz@wolter.tech>
2021-02-09 14:37:32 +0100

    update pseudo random seed.

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Quantum-Initialization experiments
 
-
--- run_exps.sh
+++ run_exps.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-python mnist.py --pseudo-init --pickle-stats
-python mnist.py --pseudo-init --pickle-stats
-python mnist.py --pseudo-init --pickle-stats
+python mnist.py --pseudo-init --pickle-stats --seed 0
+python mnist.py --pseudo-init --pickle-stats --seed 1
+python mnist.py --pseudo-init --pickle-stats --seed 2
 
 python mnist.py --pickle-stats --qbits 5
 python mnist.py --pickle-stats --qbits 5
