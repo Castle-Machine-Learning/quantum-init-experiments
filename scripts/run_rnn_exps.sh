f9909996e694697caf86db434856e01efe616144
moritz@wolter.tech <moritz@wolter.tech>
2021-02-23 18:13:16 +0100

    fix argument.

--- scripts/run_rnn_exps.sh
+++ scripts/run_rnn_exps.sh
@@ -2,5 +2,5 @@
 
 python adding_memory_rnn.py --pickle-stats --problem adding 
 python adding_memory_rnn.py --pickle-stats --problem memory
-python adding_memory_rnn.py --pickle-stats --problem adding --pseudo_init
-python adding_memory_rnn.py --pickle-stats --problem memory --pseudo_init
+python adding_memory_rnn.py --pickle-stats --problem adding --pseudo-init
+python adding_memory_rnn.py --pickle-stats --problem memory --pseudo-init
