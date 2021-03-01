5311be5f51ea3a99ca1984909b959adde7fe3a17
v0lta <moritz.wolter@scai.fraunhofer.de>
2021-03-01 13:11:07 +0100

    cnn experiment update.

--- scripts/run_cnn_exps.sh
+++ scripts/run_cnn_exps.sh
@@ -1,41 +1,23 @@
 #!/bin/sh
 echo "Running CNN experiments"
-CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init quantum --pickle-stats --seed 0 
-CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init quantum --pickle-stats --seed 1
-CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init quantum --pickle-stats --seed 2
-CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init quantum --pickle-stats --seed 3
-CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init quantum --pickle-stats --seed 4
-CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init quantum --pickle-stats --seed 5
-CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init quantum --pickle-stats --seed 6
-CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init quantum --pickle-stats --seed 7
-CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init quantum --pickle-stats --seed 8
-CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init quantum --pickle-stats --seed 9
-echo "quantum done"
 
-CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init pseudo --pickle-stats --seed 0
-CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init pseudo --pickle-stats --seed 1
-CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init pseudo --pickle-stats --seed 2
-CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init pseudo --pickle-stats --seed 3
-CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init pseudo --pickle-stats --seed 4
-CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init pseudo --pickle-stats --seed 5
-CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init pseudo --pickle-stats --seed 6
-CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init pseudo --pickle-stats --seed 7
-CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init pseudo --pickle-stats --seed 8
-CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init pseudo --pickle-stats --seed 9
-echo "pseudo-init done"
+for i in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 
+do
+   echo "Quantum-init experiment no: $i "
+   CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init quantum --pickle-stats --seed $i 
+done
+
+for i in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 
+do
+   echo "pseudorandom-init experiment no: $i "
+   CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init pseudo --pickle-stats --seed $i 
+done
 
 
-CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init pseudoquantum --pickle-stats --seed 0
-CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init pseudoquantum --pickle-stats --seed 1
-CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init pseudoquantum --pickle-stats --seed 2
-CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init pseudoquantum --pickle-stats --seed 3
-CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init pseudoquantum --pickle-stats --seed 4
-CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init pseudoquantum --pickle-stats --seed 5
-CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init pseudoquantum --pickle-stats --seed 6
-CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init pseudoquantum --pickle-stats --seed 7
-CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init pseudoquantum --pickle-stats --seed 8
-CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init pseudoquantum --pickle-stats --seed 9
-echo "pseudoquantum-init done"
-
+for i in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 
+do
+   echo "pseudoquantum-init experiment no: $i "
+   CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init pseudoquantum --pickle-stats --seed $i
+done
 
 python eval_cnn.py
