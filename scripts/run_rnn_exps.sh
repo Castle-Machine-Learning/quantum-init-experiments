e3c2c8a9830b6efdf3119d74418fd4b8e8d0901c
moritz@wolter.tech <moritz@wolter.tech>
2021-03-02 14:11:30 +0100

    fix rnn script.

--- scripts/run_rnn_exps.sh
+++ scripts/run_rnn_exps.sh
@@ -38,11 +38,3 @@ do
    echo "pseudoquantum-init experiment no: $i "
    CUBLAS_WORKSPACE_CONFIG=:4096:8 python adding_memory_rnn.py --problem memory --init pseudoquantum --pickle-stats --seed $i
 done
-
-
-
-
-python adding_memory_rnn.py --pickle-stats --problem adding 
-python adding_memory_rnn.py --pickle-stats --problem memory
-python adding_memory_rnn.py --pickle-stats --problem adding --pseudo-init
-python adding_memory_rnn.py --pickle-stats --problem memory --pseudo-init
