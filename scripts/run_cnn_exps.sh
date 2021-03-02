f8fa27d42a530d514cc70fe41f7ab6f19702823e
moritz@wolter.tech <moritz@wolter.tech>
2021-03-02 14:13:54 +0100

    cnn fix.

--- scripts/run_cnn_exps.sh
+++ scripts/run_cnn_exps.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 echo "Running CNN experiments"
 
-for i in 0 1 2 3 4 5 6 7 8 9 10
+for i in 0 1 2 3 4 5 6 7 8 9
 do
    echo "Quantum-init experiment no: $i "
    CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init quantum --pickle-stats --seed $i 
 done
 
-for i in 0 1 2 3 4 5 6 7 8 9 10
+for i in 0 1 2 3 4 5 6 7 8 9
 do
    echo "pseudorandom-init experiment no: $i "
    CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init pseudo --pickle-stats --seed $i 
 done
 
 
-for i in 0 1 2 3 4 5 6 7 8 9 10
+for i in 0 1 2 3 4 5 6 7 8 9
 do
    echo "pseudoquantum-init experiment no: $i "
    CUBLAS_WORKSPACE_CONFIG=:4096:8 python mnist_cnn.py --init pseudoquantum --pickle-stats --seed $i
